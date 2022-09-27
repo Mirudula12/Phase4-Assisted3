140daacb61348f1fe65483bb14d4e2d6717a4dd3
Mirudula12 <mirudulamit@gmail.com>
2022-09-27 23:48:09 +0530

    Added

---
+++ Assisted7/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+ 
+
+  value:string="Hi users"
+
+  cval:string="blue";
+
+
+  show=true;
+
+
+  op:string="+";
+  num1:number=2;
+  num2:number=5;
+
+
+}
