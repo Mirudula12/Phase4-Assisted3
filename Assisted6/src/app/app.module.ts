1e40efd8d085e240252319d3098cc9f690eae48d
Mirudula12 <mirudulamit@gmail.com>
2022-09-27 23:58:13 +0530

    Added

---
+++ Assisted6/src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { FormsModule } from '@angular/forms';
+import { TranspipePipe } from './transpipe.pipe';
+import { TransformPipe } from './transform.pipe';
+import { AboutComponent } from './about/about.component';
+import { RouterModule,Routes } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { ReactiveFormsModule} from '@angular/forms'
+
+const routes: Routes = [
+  {path:'register',component:RegisterComponent},
+  {path:'login',component:LoginComponent},
+  {path:'about',component:AboutComponent}
+  ];
+  
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    TranspipePipe,
+    TransformPipe,
+    AboutComponent,
+    RegisterComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot(routes),
+    ReactiveFormsModule
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
